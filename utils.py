153b3389f1c52f66f65b4003f685de4efd8e8091
Edoardo Stoppa <drdstp@gmail.com>
2021-11-21 00:53:04 -0600

    Split original dataset + added first version of utils.py

---
+++ utils.py
@@ -0,0 +1,77 @@
+import pandas as pd
+
+# Returns 2 numpy arrays of the full dataset: data, labels
+def get_dataset_full():
+    df = pd.read_csv('original_data.csv')
+    # Take the column with the label
+    labels = df['default_next_month'].to_numpy()
+    # Remove the labels from the dataset
+    data = df.drop('default_next_month', axis=1).to_numpy()
+
+    return data, labels
+
+# Returns 2 numpy arrays of the train dataset: data, labels
+def get_train():
+    df = pd.read_csv('train.csv')
+    # Take the column with the label
+    labels = df['default_next_month'].to_numpy()
+    # Remove the labels from the dataset
+    data = df.drop('default_next_month', axis=1).to_numpy()
+
+    return data, labels
+
+# Returns 2 numpy arrays of the test dataset: data, labels
+def get_test():
+    df = pd.read_csv('test.csv')
+    # Take the column with the label
+    labels = df['default_next_month'].to_numpy()
+    # Remove the labels from the dataset
+    data = df.drop('default_next_month', axis=1).to_numpy()
+
+    return data, labels
+
+# Split the dataset loaded from the specified csv file in k folds
+# Returns of tuple composed by 4 elements:
+#   1) train_data: numpy array of the data used to train the algorithm
+#   2) train_labels: numpy array of the labels associated with train_data
+#   3) valid_data: numpy array of the data used to validate the model
+#   4) valid_labels: numpy array of the labels associated with valid_data
+# Example use:
+#   folds= get_folds()
+#   for t_data, t_labels, v_data, v_labels in folds:
+#       ...
+def get_folds(k=5, csv='train.csv'):
+    df = pd.read_csv(csv)
+
+    idx = k
+    sets = []
+    # Split the dataset in k partition
+    while idx > 0:
+        sets.append(df.sample(frac=(1/idx), random_state=1))
+        df = df.drop(sets[-1].index)
+        idx -= 1
+
+    cross_val_sets = []
+    for idx in range(len(sets)):
+        # For each partition, get the the validation and train dataframe
+        validation = sets[idx]
+        sets_copy = sets.copy()
+        del sets_copy[idx]
+        train = pd.concat(sets_copy)
+
+        # Dive each set in data and labels, then convert everything to numpy arrays
+        valid_labels = validation['default_next_month'].to_numpy()
+        valid_data = validation.drop('default_next_month', axis=1).to_numpy()
+        train_labels = train['default_next_month'].to_numpy()
+        train_data = train.drop('default_next_month', axis=1).to_numpy()
+
+        # Append to list the 4 elements
+        cross_val_sets.append((train_data, train_labels, valid_data, valid_labels))
+
+    return cross_val_sets
+
+# Used only for testing
+if __name__ == '__main__':
+    #save_train_test()
+    #get_dataset_full()
+    get_folds()
