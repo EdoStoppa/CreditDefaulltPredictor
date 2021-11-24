97c60224c1abb3e8a8c655d1b1f092f596bc26ee
Edoardo Stoppa <drdstp@gmail.com>
2021-11-23 22:20:37 -0600

    Reorganized folder structure + fixed dataset imbalance

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from sklearn import metrics
+import os
 
 # Returns 2 numpy arrays of the full dataset: data, labels
 def get_dataset_full(drops=[]):
-    df = pd.read_csv('original_data.csv')
+    df = pd.read_csv(os.path.join('data', 'balanced_data.csv'))
     # Drop the selected columns
     if len(drops) > 0:
         for drop in drops:
@@ -17,7 +18,7 @@ def get_dataset_full(drops=[]):
 
 # Returns 2 numpy arrays of the train dataset: data, labels
 def get_train(drops=[]):
-    df = pd.read_csv('train.csv')
+    df = pd.read_csv(os.path.join('data', 'train_balanced.csv'))
     # Drop the selected columns
     if len(drops) > 0:
         for drop in drops:
@@ -31,7 +32,7 @@ def get_train(drops=[]):
 
 # Returns 2 numpy arrays of the test dataset: data, labels
 def get_test(drops=[]):
-    df = pd.read_csv('test.csv')
+    df = pd.read_csv(os.path.join('data', 'test_balanced.csv'))
     # Drop the selected columns
     if len(drops) > 0:
         for drop in drops:
@@ -53,7 +54,7 @@ def get_test(drops=[]):
 #   folds= get_folds()
 #   for t_data, t_labels, v_data, v_labels in folds:
 #       ...
-def get_folds(k=5, csv='train.csv', drops=[]):
+def get_folds(k=5, csv=os.path.join('data', 'train_balanced.csv'), drops=[]):
     df = pd.read_csv(csv)
     # Drop the selected columns
     if len(drops) > 0:
@@ -96,8 +97,38 @@ def get_metrics(truths, predictions):
 
     return accuracy, precision, recall, f_score
 
+def save_balanced_data():
+    df = pd.read_csv(os.path.join('data', 'original_data.csv'))
+
+    # Divide dataset in the 2 classes
+    df_0 = df[df["default_next_month"] == 0]
+    df_1 = df[df["default_next_month"] == 1]
+
+    # Shuffle them
+    df_0 = df_0.sample(frac=1).reset_index(drop=True)
+    df_1 = df_1.sample(frac=1).reset_index(drop=True)
+
+    # Get same number of sample
+    df_0 = df_0.head(6600)
+    df_1 = df_1.head(6600)
+
+    # Concatenate the 2 sub-datasets
+    df = pd.concat([df_0, df_1])
+    # Shuffle
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    # Divide in train and test dataset
+    test = df.head(3200)
+    train = df.drop(test.index)
+
+    # Save everything to a csv
+    df.to_csv(os.path.join('data', 'balanced_data.csv'), index=False)
+    train.to_csv(os.path.join('data', 'train_balanced.csv'), index=False)
+    test.to_csv(os.path.join('data', 'test_balanced.csv'), index=False)
+
 # Used only for testing
 if __name__ == '__main__':
-    get_dataset_full()
-    get_folds()
-    print(get_metrics([0,0], [0,1]))
+    #get_dataset_full()
+    #get_folds()
+
+    save_balanced_data()
